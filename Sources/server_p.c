64253aaaa3ff98c34aeb75ab549af6ac1827a350
Brandon <bdinarte@yahoo.com>
2017-09-16 11:48:32 -0700

    Implementado y funcionando el server de Pre-Thread
    Comentarios pendientes

--- Sources/server_p.c
+++ Sources/server_p.c
@@ -1,17 +1,13 @@
 #include "../Headers/server_p.h"
 
-void * run_thread2(void * arg);
-void * thread_managing(void * arg);
+void * attendClients(void *arg);
 
-List * availableThreads;
-List * busyThreads;
-List * acceptedClients;
+List * clients;
+pthread_mutex_t clientMutex = PTHREAD_MUTEX_INITIALIZER;
 
 int run_server_p(int argc, char **argv){
 
-    availableThreads = new_list();
-    busyThreads = new_list();
-    acceptedClients = new_list();
+    clients = new_list();
 
     int threadAmount = 3;
 
@@ -19,46 +15,43 @@ int run_server_p(int argc, char **argv){
 
     for(int tCount = 0; tCount < threadAmount; ++tCount){
         pthread_t thread;
-        add(availableThreads, &thread);
-    }
-
-    pthread_t threadManager;
-
-    if(pthread_create(&threadManager, 0, thread_managing, NULL) < 0 ){
-        printf("Error en el inicio del servidor.\n");
+        if (pthread_create(&thread, 0, attendClients, NULL) < 0) {
+            printf("Error de inicializacion de hilos.\n");
+        }
     }
 
     for(int n_client = 0; 1 ; ++n_client){
 
         client_info * cl = accept_client(server, n_client);
-        add(acceptedClients, cl);
-        printf("aceptado: %d\n", n_client);
+        add(clients, cl);
     }
 
 }
 
-void * thread_managing(void * arg){
+void * attendClients(void *arg){
+
     pthread_t id = pthread_self();
     pthread_detach(id);
+    client_info * cl;
 
-    for(int i = 0; 1; ++i){
-        int x = 0;
+    printf("Inicializado el hilo #%ld.\n", (long) id);
+
+    for(int i=0; 1; ++i){
+        pthread_mutex_lock(&clientMutex);
+        if(peek(clients) != 0) {
+            cl =  (client_info *) pop(clients);
+        } else {
+            cl = NULL;
+        }
+        pthread_mutex_unlock(&clientMutex);
+
+        if (cl != NULL) {
+            printf("Hilo #%ld atendiendo al cliente #%d.\n", (long) id, cl->client_id);
+            attend(cl);
+            free(cl);
+        } else {
+            printf("Hilo #%ld en SLEEP.\n", (long) id);
+        }
+        sleep(5);
     }
-
-}
-
-void * run_thread2(void * arg){
-
-    pthread_t id = pthread_self();
-    pthread_detach(id);
-
-    client_info * cl =  (client_info *) arg;
-    printf("Hilo %ld atendiendo al cliente #%d\n", (long) id, cl->client_id);
-
-    attend(cl);
-
-    free(cl);
-
-    pthread_exit(0);
-
 }
